f35eaca4cd4af4af3a01e2a573a6b63ab7680c98
HyeonU-Jo <chw3448@gmail.com>
2023-01-19 13:53:57 +0900

    CSS 수정

--- js/greetings.js
+++ js/greetings.js
@@ -6,26 +6,26 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
 
-  const username = loginInput.value;
+    const username = loginInput.value;
 
-  localStorage.setItem(USERNAME_KEY, username);
+    localStorage.setItem(USERNAME_KEY, username);
 
-  paintGreetings(username);
+    paintGreetings(username);
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `오늘도 좋은 하루 되세요, ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername == null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreetings(savedUsername);
+    paintGreetings(savedUsername);
 }
--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,21 @@
 function onGeoOk(position) {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
-  const weather = document.querySelector("#weather span:first-child");
-  const city = document.querySelector("#weather span:last-child");
-  const API_KEY = "05ca6b667b962f937cb9a07f0c293951";
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    const API_KEY = "05ca6b667b962f937cb9a07f0c293951";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-      city.innerText = data.name;
-    });
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+            city.innerText = data.name;
+        });
 }
 
 function onGeoError() {
-  alert("현 위치를 찾을 수 없습니다.");
+    alert("현 위치를 찾을 수 없습니다.");
 }
 
 console.log(navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError));
